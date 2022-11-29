9060de29463cd04587b6d7f96fa22fddf865059b
CatUnderGlue <warmonds1996@gmail.com>
2022-11-29 12:47:13 +0500

    Task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Домашнее задание
+        // Задание 1
+        int clientOS = 0;
+        switch (clientOS){
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке");
+                break;
+            default:
+                System.out.println("Не удалось распознать вашу операционную систему");
+        }
     }
 }
